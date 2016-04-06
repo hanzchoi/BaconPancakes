6f620f7569209906cf76c654632f89257c140a5e
personabe1984 <personabe1984@gmail.com>
2016-04-05 20:48:14 -0400

    Added the project

---
+++ app/src/main/java/personabe1984/nyc/baconpancakes/ListFragment.java
@@ -0,0 +1,21 @@
+package personabe1984.nyc.baconpancakes;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    // This is what will be visible in our fragment
+    // Need to return a view that represents our fragment list representing our fragment ist layout
+    // Layout inflater is what we convert sml to View
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container);
+        return view;
+    }
+}
