0c9e207e5dc5a4bb9ac6674ece5a39a03b059346
personabe1984 <personabe1984@gmail.com>
2016-04-06 22:33:34 -0400

    generating all the views using our adapter in our ListFragment class

--- app/src/main/java/personabe1984/nyc/baconpancakes/ListFragment.java
+++ app/src/main/java/personabe1984/nyc/baconpancakes/ListFragment.java
@@ -3,6 +3,8 @@ package personabe1984.nyc.baconpancakes;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,17 @@ public class ListFragment extends Fragment {
     // Layout inflater is what we convert sml to View
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);  // attached the adapter to the recyclerView
+
+        //layout manager is responsible or determining where to lace the views
+        // as well as when to reuse a view that's no longer visible
+        // To make sure Recycler view to act like a list we can use built in linearManager
+        // Fragment always have to access their activity through the getActivity
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
         return view;
     }
 }
