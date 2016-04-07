a3aac80a6543d2de6cc362b716d7169c2d3f288b
personabe1984 <personabe1984@gmail.com>
2016-04-06 22:17:52 -0400

    made a list using RecyclerView and Adapters

---
+++ app/src/main/java/personabe1984/nyc/baconpancakes/ListAdapter.java
@@ -0,0 +1,54 @@
+package personabe1984.nyc.baconpancakes;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        return new ListViewHolder(view);
+    }
+
+
+    // this is where we should update the view and our view holder to reflect
+    // which position it's on the screen
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((ListViewHolder) holder).bindView(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return Recipes.names.length;
+    }
+
+    private class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView mTextView;
+        private ImageView mImageView;
+
+        public ListViewHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView)itemView.findViewById(R.id.itemText);
+            mImageView = (ImageView)itemView.findViewById(R.id.itemImage);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bindView(int position ){
+            // in this method we need to update the text view and image view to display the right recipe
+            mTextView.setText(Recipes.names[position]);
+            mImageView.setImageResource(Recipes.resourceIds[position]);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
