ec5226669b65f6e863d62a05447785167f08e0ab
personabe1984 <personabe1984@gmail.com>
2016-04-11 14:38:25 -0400

    Fixed the rotation problem in the fragment and changed the title name to the currrent Recipe && changed the app name when we hit the back button to our main activity

--- app/src/main/java/personabe1984/nyc/baconpancakes/MainActivity.java
+++ app/src/main/java/personabe1984/nyc/baconpancakes/MainActivity.java
@@ -5,16 +5,19 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnRecipeSelectedInterface{
+    public static final String LIST_FRAGMENT = "list_fragment";
+    public static final String VIEWPAGER_FRAGMENT = "viewpager_fragment";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment savedFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.placeHolder);
+        ListFragment savedFragment = (ListFragment) getSupportFragmentManager()
+                .findFragmentByTag(LIST_FRAGMENT);
         if(savedFragment == null){
 
             ListFragment fragment = new ListFragment();
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnRe
             FragmentManager fragmentManager = getSupportFragmentManager();
 
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.placeHolder,fragment);
+            fragmentTransaction.add(R.id.placeHolder,fragment,LIST_FRAGMENT);
 
             fragmentTransaction.commit();
         }
@@ -32,12 +35,13 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnRe
 
     @Override
     public void onListRecipeSelected(int index) {
-        Toast.makeText(MainActivity.this, Recipes.names[index], Toast.LENGTH_SHORT).show();
-
         ViewPagerFragment fragment = new ViewPagerFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ViewPagerFragment.KEY_RECIPE_INDEX,index);
+        fragment.setArguments(bundle);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.placeHolder,fragment);
+        fragmentTransaction.replace(R.id.placeHolder,fragment,VIEWPAGER_FRAGMENT);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
 
