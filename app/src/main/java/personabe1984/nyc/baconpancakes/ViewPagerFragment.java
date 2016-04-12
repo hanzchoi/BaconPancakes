9750bbeb7d3973111b2c8fcc1f4cc635632ae840
personabe1984 <personabe1984@gmail.com>
2016-04-12 14:50:51 -0400

    finished up the basic visual aspect of the ViewPager in our fragment

--- app/src/main/java/personabe1984/nyc/baconpancakes/ViewPagerFragment.java
+++ app/src/main/java/personabe1984/nyc/baconpancakes/ViewPagerFragment.java
@@ -4,6 +4,8 @@ package personabe1984.nyc.baconpancakes;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,24 @@ public class ViewPagerFragment extends Fragment {
         //Toast.makeText(getActivity(), Recipes.names[index], Toast.LENGTH_SHORT).show();
         getActivity().setTitle(Recipes.names[index]);
         View view = inflater.inflate(R.layout.fragment_viewpager,container,false);
+
+        final IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        final DirectionsFragment directionsFragment = new DirectionsFragment();
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        // When we deal with fragment with in the fragment we need to use ChildfragmentManager
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return position == 0 ? ingredientsFragment : directionsFragment;
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
         return view;
     }
 
