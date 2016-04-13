1ddcd349f3636185dd78054f23a159d875c9ec5b
personabe1984 <personabe1984@gmail.com>
2016-04-12 22:34:06 -0400

    Added the tap So excited

--- app/src/main/java/personabe1984/nyc/baconpancakes/ViewPagerFragment.java
+++ app/src/main/java/personabe1984/nyc/baconpancakes/ViewPagerFragment.java
@@ -3,6 +3,7 @@ package personabe1984.nyc.baconpancakes;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -33,10 +34,18 @@ public class ViewPagerFragment extends Fragment {
             }
 
             @Override
+            public CharSequence getPageTitle(int position) {
+                return position == 0 ? "Ingredients" : "Directions";
+            }
+
+            @Override
             public int getCount() {
                 return 2;
             }
         });
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
 
         return view;
     }
